70d92ee1e46fd207805999308b91d41c6a0b28a5
gautami2i <gautam.vijayan@ideas2it.com>
2024-05-17 05:01:34 +0530

    Implemented the sse

--- src/Landing/service/generateStory.ts
+++ src/Landing/service/generateStory.ts
@@ -1,23 +1,27 @@
 import { axiosInstance } from "../../common/http";
 
-const BASE_URL = 'http://192.168.22.207:8080'; 
+const BASE_URL = "http://192.168.22.207:8080/api/legacy/";
 
 export const generateStory = async (payload: {
   userName: string;
   legacyTech: string;
   legacy_code: any;
 }) => {
-  const token = localStorage.getItem('token')
-  const url = `/api/legacy/generate-story-brd?username=${payload.userName}&legacyTech=${payload.legacyTech}`;
+  const token = localStorage.getItem("token");
+  const url = `generate-story-brd?username=${payload.userName}&legacyTech=${payload.legacyTech}`;
   const config = {
     headers: {
-      'content-type': 'multipart/form-data',
+      "content-type": "multipart/form-data",
       Accept: "*/*",
-      Authorization: `Bearer ${token}`
-    }
-  }
+      Authorization: `Bearer ${token}`,
+    },
+  };
   try {
-    const response = await axiosInstance.post(`${BASE_URL}${url}`, payload.legacy_code, config);
+    const response = await axiosInstance.post(
+      `${BASE_URL}${url}`,
+      payload.legacy_code,
+      config
+    );
 
     if (response) {
       return response;
@@ -26,3 +30,4 @@ export const generateStory = async (payload: {
     throw error.response?.data || error.message;
   }
 };
+
--- src/auth/authService.service.ts
+++ src/auth/authService.service.ts
@@ -1,32 +1,46 @@
-import { axiosInstance } from '../common/http';
-
+import { jwtDecode } from "jwt-decode";
+import { axiosInstance, getAuthToken } from "../common/http";
 
 export const registerUser = async (payload) => {
-    const url = `/auth/register`
-    try {
-        const response = await axiosInstance.post(url, payload);
-        if (response?.status === 200) {
-            console.log(response)
-            return response
-        } else {
-            throw response
-        }
-    } catch (error) {
-        throw error.response.data;
+  const url = `auth/register`;
+  try {
+    const response = await axiosInstance.post(url, payload);
+    if (response?.status === 200) {
+      console.log(response);
+      return response;
+    } else {
+      throw response;
     }
-}
+  } catch (error) {
+    throw error.response.data;
+  }
+};
 
 export const loginUser = async (payload) => {
-    const url = `/auth/login`
-    try {
-        const response = await axiosInstance.post(url, payload)
-        if (response?.status === 200) {
-            console.log(response)
-            return response
-        } else {
-            throw response
-        }
-    } catch (error) {
-        throw error;
+  const url = `auth/login`;
+  try {
+    const response = await axiosInstance.post(url, payload);
+    if (response?.status === 200) {
+      console.log(response);
+      return response;
+    } else {
+      throw response;
     }
-}
+  } catch (error) {
+    throw error.response.data;
+  }
+};
+
+export const getUser = async () => {
+  let username = jwtDecode(getAuthToken()).sub;
+  const url = `auth/user?userName=${username}`;
+  try {
+    const response = await axiosInstance.get(url);
+    if (response) {
+      return response;
+    }
+  } catch (error) {
+    throw error.response.data;
+  }
+};
+

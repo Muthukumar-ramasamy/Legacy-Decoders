f0d4788de5d39dbfa37a9c38baeaea1bc2394a4f
Sakthi Annamali Sugumar <sakthiannamali.sugumar@ideas2it.com>
2024-05-17 14:24:20 +0530

    Fix generated story

---
+++ src/Landing/components/DrawerPopup.tsx
@@ -0,0 +1,105 @@
+import { Box, Typography, Button, Drawer, Stack } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import { downloadFile } from "../util/commonUtil";
+import downloadIcon from "../../assets/download.svg";
+import { useState } from "react";
+
+export const DrawerPopup = (props: any) => {
+  const [toast, setToast] = useState<any>();  
+  return (
+    <Drawer
+      sx={{
+        ">.MuiPaper-root": {
+          height: "85vh",
+          p: 2,
+          borderTopLeftRadius: "10px",
+          borderTopRightRadius: "10px",
+          boxShadow: "0px 0px 15px rgba(0, 0, 0, 0.4)",
+          whiteSpace: "pre-wrap",
+        },
+      }}
+      anchor={"bottom"}
+      open={true}
+      onClose={() => props?.close()}
+      BackdropProps={{ invisible: true }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          height: "100%",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            width: "100%",
+            marginBottom: "10px",
+          }}
+        >
+          <Typography
+            sx={{
+              fontWeight: "600",
+            }}
+            variant={"h6"}
+          >
+            {props?.header}
+          </Typography>
+          <Stack
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+            }}
+          >
+            {props?.isDownloadEnable && (
+              <Stack
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  cursor: "pointer",
+                  mr: 2,
+                  backgroundColor: "white",
+                  border: "solid 1px",
+                  borderColor: "blue",
+                  padding: "8px",
+                  borderRadius: "10px",
+                }}
+                onClick={() => downloadFile(props?.url)}
+              >
+                <Typography
+                  sx={{ color: "blue", textSize: "3px", marginRight: "5px" }}
+                  component="div"
+                >
+                  Download
+                </Typography>
+                <Stack sx={{}}>
+                  <img src={downloadIcon} alt="download" />
+                </Stack>
+              </Stack>
+            )}
+            <Button
+              sx={{
+                textTransform: "capitalize",
+                color: "gray",
+                "&:hover": {
+                  backgroundColor: "#f1f1f1",
+                },
+              }}
+              onClick={() => props?.close()}
+              endIcon={<CloseIcon />}
+              variant={"text"}
+            >
+              {"Close"}
+            </Button>
+          </Stack>
+        </Box>
+        <Stack>{props.element}</Stack>
+      </Box>
+    </Drawer>
+  );
+};
